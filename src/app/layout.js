11ac843682612ef9fc5cc1c75d782e925dc3e4c1
Vasily Loban <34033416+vasily88@users.noreply.github.com>
2024-10-20 21:56:57 +0300

    favicon change

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,9 +8,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* <head>
-        <link rel="icon" href="./favicon.ico" type="image/x-icon" sizes="16x16" />
-      </head> */}
       <body>
         {children}
       </body>
