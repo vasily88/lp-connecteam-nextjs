7e92eb977a08bafa2975b48a81a50bd9d161ea20
Vasily Loban <34033416+vasily88@users.noreply.github.com>
2024-10-20 21:50:29 +0300

    export to out

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,4 @@
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -21,9 +8,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      {/* <head>
+        <link rel="icon" href="./favicon.ico" type="image/x-icon" sizes="16x16" />
+      </head> */}
+      <body>
         {children}
       </body>
     </html>
