6fe7a206015f6a392e938cfb2071857c907ef119
Vasily Loban <34033416+vasily88@users.noreply.github.com>
2024-10-21 22:11:12 +0300

    add main image to hebo section

--- src/app/components/HeboSection/HeboSection.jsx
+++ src/app/components/HeboSection/HeboSection.jsx
@@ -1,31 +1,56 @@
 'use client'
 import { useEffect, useState } from 'react';
-import { fetchData } from '../../utils/fetchData';
+import useDeviceType from '../../utils/useDeviseType';
+import { fetchDataContent } from '../../utils/fetchData';
+import { fetchImages } from '../../utils/fetchData';
+import Image from 'next/image';
 import './HeboSection.css';
 
 const HeboSection = () => {
 
-    const [data, setData] = useState(null);
+    const isMobile = useDeviceType();
+
+    const [dataContent, setDataContent] = useState(null);
+    const [dataImages, setDataImages] = useState(null);
 
     useEffect(() => {
-        async function loadContent(){
-            const urls = [
-                'https://connecteam.com/static/frontend-home-task/data/home.json'
-            ]
-            const result = await fetchData(urls);
-            setData(result);
+
+        // Load Content
+        async function loadContent() {
+            const url = 'https://connecteam.com/static/frontend-home-task/data/home.json';
+            const result = await fetchDataContent(url);
+            setDataContent(result);
         }
         loadContent();
-    },[])
 
-  return (
-    <div>
-        HeboSection
-        <div>
-            {data ? JSON.stringify(data) : null}
-        </div>
-    </div>
-  )
+        // Load Images
+        async function loadImages() {
+            const urls = [
+                'home-small',
+                'home-large',
+            ];
+            const result = await fetchImages(urls);
+            setDataImages(result);
+        }
+        loadImages();
+    }, [])
+
+    return (
+        <section className='heboSection'>
+            {dataImages ?
+                <Image
+                    src={isMobile ? dataImages.image1 : dataImages.image2}
+                    alt="delivery man"
+                    fill
+                    style={{ objectFit: 'cover' }}
+                />
+                : null}
+
+            <div>
+                {/* {dataContent ? JSON.stringify(dataContent) : null} */}
+            </div>
+        </section>
+    )
 }
 
 export default HeboSection
--- src/app/utils/fetchData.js
+++ src/app/utils/fetchData.js
@@ -1,33 +1,55 @@
-// export async function fetchData(url) {
-//   try {
-//     const response = await fetch(url);
-//     if (!response.ok) {
-//       throw new Error(`Error: ${response.status} - ${response.statusText}`);
-//     }
-//     const data = await response.json();
-//     return data;
-//   } catch (error) {
-//     console.error("Fetching data failed:", error);
-//     return null;
-//   }
-// }
-
-export async function fetchData(urls) {
+export async function fetchDataContent(url) {
   try {
-    const responses = await Promise.all(urls.map(url => fetch(url)));
-
-    // Check if all responses are ok
-    const allOk = responses.every(response => response.ok);
-    if (!allOk) {
-      const errorResponse = responses.find(response => !response.ok);
-      throw new Error(`Error: ${errorResponse.status} - ${errorResponse.statusText}`);
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} - ${response.statusText}`);
     }
-
-    const data = await Promise.all(responses.map(response => response.json()));
+    const data = await response.json();
     return data;
   } catch (error) {
     console.error("Fetching data failed:", error);
     return null;
   }
 }
+
+export async function fetchImages(names) {
+
+  const urlsName = {
+    "home-small":"https://connecteam.com/static/frontend-home-task/jpg/home-small.jpg",
+    "home-large":"https://connecteam.com/static/frontend-home-task/jpg/home-large.jpg",
+    "repudiandae-large":"https://connecteam.com/static/frontend-home-task/jpg/repudiandae-large.jpg",
+    "repudiandae-small":"https://connecteam.com/static/frontend-home-task/jpg/repudiandae-small.jpg",
+    "sit-et-enim-large":"https://connecteam.com/static/frontend-home-task/jpg/sit-et-enim-large.jpg",
+    "sit-et-enim-small":"https://connecteam.com/static/frontend-home-task/jpg/sit-et-enim-small.jpg",
+    "dolore-ipsum-large":"https://connecteam.com/static/frontend-home-task/jpg/dolore-ipsum-large.jpg",
+    "dolore-ipsum-small":"https://connecteam.com/static/frontend-home-task/jpg/dolore-ipsum-small.jpg",
+    "praesentium-aspernatur-large":"https://connecteam.com/static/frontend-home-task/jpg/praesentium-aspernatur-large.jpg",
+    "praesentium-aspernatur-small":"https://connecteam.com/static/frontend-home-task/jpg/praesentium-aspernatur-small.jpg"
+  }
+
+  try {
+    const urls = names.map(name => urlsName[name]);
+    const imagePromises = urls.map(async (url, index) => {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} - ${response.statusText}`);
+      }
+      return response.url; 
+    });
+
+    const imageSrcs = await Promise.all(imagePromises);
+    const imageUrls = imageSrcs.reduce((acc, src, index) => {
+      acc[`image${index + 1}`] = src;
+      return acc;
+    }, {});
+
+    return imageUrls;
+  } catch (error) {
+    console.error("Fetching images failed:", error);
+    return null;
+  }
+}
+
+
+
 
